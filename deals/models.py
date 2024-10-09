c2bdf23c1bb399557f23157fb12a466ef74a4837
gaidds <ad.gaidamovich@gmail.com>
2024-10-09 11:35:15 +0200

    added real_close_date in Deals models so that when deal stage updated to CLOSED then the date would be saved. Id ctage changed from CLOSED then it will be NULL again

--- deals/models.py
+++ deals/models.py
@@ -2,6 +2,7 @@ import arrow
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import pgettext_lazy
+from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
 from accounts.models import Tags, Account
@@ -18,7 +19,8 @@ from common.utils import (
 
 
 class Deal(BaseModel):
-    name = models.CharField(pgettext_lazy("Name of Deal", "Name"), max_length=64)
+    name = models.CharField(pgettext_lazy(
+        "Name of Deal", "Name"), max_length=64)
     contacts = models.ManyToManyField(Contact)
     assigned_to = models.ManyToManyField(
         Profile, related_name="deal_assigned_to"
@@ -30,12 +32,18 @@ class Deal(BaseModel):
         blank=True,
         null=True,
     )
-    website = models.CharField(_("Website"), max_length=255, blank=True, null=True)
-    stage = models.CharField(pgettext_lazy("Stage of Deal", "Stage"), max_length=64, choices=STAGES)
-    deal_source = models.CharField(_("Source of Deal"), max_length=255, choices=SOURCES, blank=True, null=True)
-    industry = models.CharField(_("Industry Type"), max_length=255, choices=INDCHOICES, blank=True, null=True)
-    currency = models.CharField(max_length=3, choices=CURRENCY_CODES, blank=True, null=True)
-    value = models.DecimalField(_("Deal Value"), decimal_places=2, max_digits=12, blank=True, null=True)
+    website = models.CharField(
+        _("Website"), max_length=255, blank=True, null=True)
+    stage = models.CharField(pgettext_lazy(
+        "Stage of Deal", "Stage"), max_length=64, choices=STAGES)
+    deal_source = models.CharField(
+        _("Source of Deal"), max_length=255, choices=SOURCES, blank=True, null=True)
+    industry = models.CharField(
+        _("Industry Type"), max_length=255, choices=INDCHOICES, blank=True, null=True)
+    currency = models.CharField(
+        max_length=3, choices=CURRENCY_CODES, blank=True, null=True)
+    value = models.DecimalField(
+        _("Deal Value"), decimal_places=2, max_digits=12, blank=True, null=True)
     probability = models.IntegerField(default=0, blank=True, null=True)
     closed_by = models.ForeignKey(
         Profile,
@@ -45,26 +53,28 @@ class Deal(BaseModel):
         related_name="deal_closed_by",
     )
     close_date = models.DateField(default=None, null=True)
+    real_close_date = models.DateField(default=None, null=True)
     description = models.TextField(blank=True, null=True)
     tags = models.ManyToManyField(Tags, blank=True)
     org = models.ForeignKey(
         Org, on_delete=models.SET_NULL, null=True, blank=True, related_name="deal_org"
     )
-    country = models.CharField(max_length=3, choices=COUNTRIES, blank=True, null=True)
+    country = models.CharField(
+        max_length=3, choices=COUNTRIES, blank=True, null=True)
+
     class Meta:
         verbose_name = "Deal"
         verbose_name_plural = "Deals"
         db_table = "deal"
-        ordering = ("-created_at",)    
-    
-    
+        ordering = ("-created_at",)
+
     def __str__(self):
         return f"{self.name}"
-    
+
     @property
     def created_on_arrow(self):
         return arrow.get(self.created_at).humanize()
-    
+
     @property
     def get_team_users(self):
         team_user_ids = list(self.teams.values_list("users__id", flat=True))
@@ -84,4 +94,12 @@ class Deal(BaseModel):
         user_ids = set(assigned_user_ids) - set(team_user_ids)
         return Profile.objects.filter(id__in=list(user_ids))
 
+    def save(self, *args, **kwargs):
+        # Check if the stage is changing to 'CLOSED WON' or 'CLOSED LOST'
+        if self.pk:  # Only if this is an existing instance
+            original = Deal.objects.get(pk=self.pk)
+            if original.stage != self.stage and self.stage in ["CLOSED WON", "CLOSED LOST"]:
+                self.real_close_date = timezone.now().date()  # Set to current date
 
+        # Call the save method of the base class to handle created_by and updated_by
+        super(Deal, self).save(*args, **kwargs)
